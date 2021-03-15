d446f5075198f2e7127d3fa776758c0f6bf88320
sanminpark <parksm@stanford.edu>
2021-03-16 00:09:34 +0900

    PageView - debugged and introduced some code optimizations

--- BunnyWorld/app/src/main/java/edu/stanford/cs108/PageView.java
+++ BunnyWorld/app/src/main/java/edu/stanford/cs108/PageView.java
@@ -21,24 +21,27 @@ public class PageView extends View {
     List<Shape> shapeList = currentPage.getShapeList();
     Shape currentShape = null;
 
+    boolean inInventory;
 
-    final Paint linePaint;
+    // constants and variables related to the Inventory line
+
+    // how many px above/below the Inventory line do you want to reposition the shape, if the user drags it onto the line?
+    static final int INVENTORY_LINE_BUFFER = 5;
+    // how far down the page should the inventory line be?
+    static final float INVENTORY_LINE_POSITION_RATIO = (float)4 / 5;
+    final Paint linePaint; // TODO: MEETING TOPIC: why is this "final"?
     float lineHeight;
 
+    // TODO: remove this line once we can automatically get rectColor from the RectShape object itself.
+    Paint rectColor = new Paint(Color.BLACK);
 
-    //    DELETE
-    float x, y;
-    Paint shapePaint;
-    float SQUARE_SIZE = 30.0f;
 
-    boolean inInventory;
-//    DELETE
+
 
     public PageView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        this.linePaint = new Paint();
-        this.shapePaint = new Paint();
+        this.linePaint = new Paint(); // TODO: (related to line 32) we can move this into init() if it's not final
         init();
     }
 
@@ -46,45 +49,34 @@ public class PageView extends View {
         linePaint.setColor(Color.BLACK);
         linePaint.setStrokeWidth(5);
 
-        shapePaint.setColor(Color.rgb(140,21,21));
         inInventory = false;
-        this.lineHeight = 0;
+        this.lineHeight = 0; // this might not be necessary; discussion topic
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        this.lineHeight = canvas.getHeight() * 4 / 5;
-        canvas.drawLine(0, this.lineHeight, canvas.getWidth(), this.lineHeight, linePaint);
-
-        canvas.drawRect(x-SQUARE_SIZE, y-SQUARE_SIZE,
-                x+SQUARE_SIZE, y+SQUARE_SIZE, shapePaint);
+        this.lineHeight = getHeight() * INVENTORY_LINE_POSITION_RATIO;
+        canvas.drawLine(0, this.lineHeight, getWidth(), this.lineHeight, linePaint);
 
         // Sammy's edits
 
         for (Shape shape : currentPage.getShapeList()) {
-            // print the shapes
-
-            // System.out.println(shape);
-
+            // TODO: add if statements for all types of Shape. Alternatively,
+            //  have a method in each of the classes so that they can draw themselves.
+            //  This might be a better solution because it allows Editor to avoid duplicating code.
             if (shape instanceof RectShape) {
-                // print the rectangle
 
-                // System.out.println("This shape is a RectShape");
-                // System.out.println(shape.getX());
-                // System.out.println(shape.getY());
-
-                Paint rectColor = new Paint(Color.BLACK); // TODO: change linePaint to receive color from RectShape
+                // TODO: change linePaint to receive color from RectShape
                 canvas.drawRect(shape.getX(), shape.getY(),
                         shape.getX()+shape.getWidth(), shape.getY()+shape.getHeight(), rectColor);
-
             }
         }
     }
 
     // these float variables are only used in this function
-    float offsetX;
-    float offsetY;
+    private float offsetX;
+    private float offsetY;
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
@@ -104,19 +96,21 @@ public class PageView extends View {
                 }
                 break;
             case MotionEvent.ACTION_UP:
-                float shapeY = currentShape.getY();
-                float shapeHeight = currentShape.getHeight();
-                if (lineHeight >= shapeY && lineHeight <= shapeY + shapeHeight) { // shape on inventory line
-                    if (lineHeight >= shapeY + shapeHeight / 2) { // shape moreso in page
-                        currentShape.setY(lineHeight - shapeHeight - 5);
-                        // not in inventory
-                    } else { // moreso in inventory
-                        currentShape.setY(lineHeight + 5);
-                        // in inventory
+                if (currentShape != null) {
+                    float shapeY = currentShape.getY();
+                    float shapeHeight = currentShape.getHeight();
+                    if (lineHeight >= shapeY && lineHeight <= shapeY + shapeHeight) { // shape on inventory line
+                        if (lineHeight >= shapeY + shapeHeight / 2) { // shape moreso in page
+                            currentShape.setY(lineHeight - shapeHeight - INVENTORY_LINE_BUFFER);
+                            // not in inventory
+                        } else { // moreso in inventory
+                            currentShape.setY(lineHeight + INVENTORY_LINE_BUFFER);
+                            // in inventory
+                        }
                     }
+                    this.inInventory = currentShape.getY() > lineHeight;
+                    currentShape = null; // deselect the shape
                 }
-                this.inInventory = y > lineHeight;
-                currentShape = null; // deselect the shape
                 break;
         }
         invalidate();
