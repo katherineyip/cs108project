c837312bf12952d42416d73b12c48d738aed0adf
jpalisch <55963957+jpalisch@users.noreply.github.com>
2021-03-15 21:31:19 -0700

    Add files via upload

--- BunnyWorld/app/src/main/java/edu/stanford/cs108/RectShapeActivity.java
+++ BunnyWorld/app/src/main/java/edu/stanford/cs108/RectShapeActivity.java
@@ -6,9 +6,15 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RectShapeActivity extends AppCompatActivity {
     // Data
@@ -24,15 +30,39 @@ public class RectShapeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rect_shape);
 
+        //Populate our Spinners
+        Spinner eventSpinner = findViewById(R.id.evSpin2);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.events, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        eventSpinner.setAdapter(adapter);
+
+        Spinner actionSpinner = findViewById(R.id.acSpin2);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.actions, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        actionSpinner.setAdapter(adapter2);
+
+        //Populate Spinner from Page list
+        Spinner pageSpinner = findViewById(R.id.inputRectPage);
+        List<Page> pages = game.pageList;
+        ArrayList<String> pageNames = new ArrayList<String>();
+        pageNames.add("Pages");
+        for(Page p : pages){
+            pageNames.add(p.pageName);
+        }
+        ArrayAdapter<String> adapter4 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, pageNames);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        pageSpinner.setAdapter(adapter4);
+
         // Set up onClickListener on "Submit" to bring user back to EditorActivity
         buttonSetRect = findViewById(R.id.buttonSetRect);
         buttonSetRect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onSubmit(view); // Create new rectShape
-
-                Intent intent = new Intent(RectShapeActivity.this, EditorActivity.class);
-                startActivity(intent); // Direct user back to EditorActivity
+                if (errorCheck()) {
+                    Intent intent = new Intent(RectShapeActivity.this, EditorActivity.class);
+                    startActivity(intent); // Direct user back to EditorActivity
+                }
             }
         });
 
@@ -50,36 +80,107 @@ public class RectShapeActivity extends AppCompatActivity {
 
     public void onSubmit(View view) {
         EditText inputRectShapeName = findViewById(R.id.inputImageShapeName);
-        EditText inputRectPage = findViewById(R.id.inputRectPage); //TODO: Change to dropdown later
+        Spinner inputRectPage = findViewById(R.id.inputRectPage); //TODO: Change to dropdown later
         CheckBox checkboxRectIsHidden = findViewById(R.id.checkboxRectIsHidden);
         CheckBox checkboxRectIsMovable = findViewById(R.id.checkboxRectIsMovable);
         CheckBox checkboxRectIsInventory = findViewById(R.id.checkboxRectIsInventory);
-        EditText editRectXPosition = findViewById(R.id.editRectXPosition);
-        EditText editRectYPosition = findViewById(R.id.editRectYPosition);
-        EditText editRectWidth = findViewById(R.id.editRectWidth);
-        EditText editRectHeight = findViewById(R.id.editRectHeight);
+        EditText editRectLeftPosition = findViewById(R.id.editRectLeftPosition);
+        EditText editRectRightPosition = findViewById(R.id.editRectRightPosition);
+        EditText editRectTopPosition = findViewById(R.id.editRectTopPosition);
+        EditText editRectBottomPosition = findViewById(R.id.editRectBottomPosition);
 
-        float x = Float.parseFloat(String.valueOf(editRectXPosition.getText()));
-        float y = Float.parseFloat(String.valueOf(editRectYPosition.getText()));
-        // TODO: if the user wants the item to be in Inventory, then we should throw an error if the (x,y) is invalid (i.e. not within the Inventory box)
+        Spinner actionSpinner = findViewById(R.id.acSpin2);
+        Spinner eventSpinner = findViewById(R.id.evSpin2);
+        EditText scriptScript =findViewById(R.id.rectScript);
+        EditText scriptTarget = findViewById(R.id.rectTarget);
 
-        float width = Float.parseFloat(String.valueOf(editRectWidth.getText()));
-        float height = Float.parseFloat(String.valueOf(editRectHeight.getText()));
+        String script = "";
+        if(eventSpinner.getSelectedItemPosition()!= 0){
+            script = eventSpinner.getSelectedItem().toString();
+
+            if(script.equals("on drop")){
+                script += " ";
+                script += scriptTarget.getText().toString();
+            }
+            script += " ";
+            script += actionSpinner.getSelectedItem().toString();
+            script += " ";
+            script += scriptScript.getText().toString();
+            script += ";";
+
+        }
+
+
+
+        int index = inputRectPage.getSelectedItemPosition();
+        if(index == 0) {
+            System.out.println("Must Select Page to Add To");
+            return;
+        }
+
+        String xs1 = editRectLeftPosition.getText().toString();
+        String xs2 = editRectRightPosition.getText().toString();
+        String ys1 = editRectTopPosition.getText().toString();
+        String ys2 = editRectBottomPosition.getText().toString();
+        int x1 = Integer.valueOf(xs1);
+        int x2 = Integer.valueOf(xs2);
+        int y1 = Integer.valueOf(ys1);
+        int y2 = Integer.valueOf(ys2);
+
+        int width = x2-x1;
+        int height = y2 - y1;
 
         RectShape newRect = new RectShape(Color.GRAY,
                 inputRectShapeName.getText().toString(),
                 checkboxRectIsHidden.isChecked(),
                 checkboxRectIsMovable.isChecked(),
-                checkboxRectIsInventory.isChecked(),
-                "shape script",
-                x, y, width, height); // TODO: Replace later
-        String destination = inputRectPage.getText().toString(); // TODO: Need to get Page instead of string
+                checkboxRectIsInventory.isChecked(), script,
+                (float)x1, (float)y1, (float)width, (float)height); // TODO: Replace later
+
+
+
+
+
+        //Page to add to
+        Page addTo = null;
+        String destination = inputRectPage.getSelectedItem().toString();
+        //loop through the page list to find page with same name
+        for(Page p: game.getPageList()){
+            if (p.getPageName().equals(destination)){
+                addTo = p;
+            }
+        }
+
+        //If there is not page with this name
+        if(addTo.equals(null)){
+            System.out.println("Not a valid page.");
+            return;
+        }
 
         if (checkboxRectIsInventory.isChecked()) {
             game.addInventory(newRect);
         } else {
-            game.getCurrentPage().addShape(newRect); //TODO: Need to pass in user's page selection
+            addTo.addShape(newRect); //TODO: Need to pass in user's page selection
         }
-        System.out.println("Singleton after adding rect " + game.getCurrentPage().getNumShapesOnPage());
+        System.out.println("Singleton after adding rect " + addTo.getNumShapesOnPage());
+    }
+
+    public boolean errorCheck(){
+        EditText inputRectShapeName = findViewById(R.id.inputImageShapeName);
+        Spinner inputRectPage = findViewById(R.id.inputRectPage);
+
+
+        if(inputRectShapeName.getText().toString().equals("")){
+            Toast.makeText(this, "Must Give Text Name", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if(inputRectPage.getSelectedItemPosition() == 0){
+            Toast.makeText(this, "Must Select Page", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+
+        return true;
     }
 }
--- BunnyWorld/app/src/main/java/edu/stanford/cs108/TextShapeActivity.java
+++ BunnyWorld/app/src/main/java/edu/stanford/cs108/TextShapeActivity.java
@@ -3,88 +3,194 @@ package edu.stanford.cs108;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TextShapeActivity extends AppCompatActivity {
+
     // Data
     SingletonData singletonData = SingletonData.getInstance(); // Store list of games in memory
     Game game = singletonData.getCurrentGame();
 
     // UI
-    Button buttonSetText;
-    Button buttonCancelText;
-    // TODO: Need UI element for setting scripts
+    Button buttonSetTxt;
+    Button buttonCancelTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_shape);
 
+        //Populate our Spinners
+        Spinner eventSpinner = findViewById(R.id.evSpin3);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.events, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        eventSpinner.setAdapter(adapter);
+
+        Spinner actionSpinner = findViewById(R.id.acSpin3);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.actions, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        actionSpinner.setAdapter(adapter2);
+
+        //Populate Spinner from Page list
+        Spinner pageSpinner = findViewById(R.id.inputTextPage);
+        List<Page> pages = game.pageList;
+        ArrayList<String> pageNames = new ArrayList<String>();
+        pageNames.add("Pages");
+        for(Page p : pages){
+            pageNames.add(p.pageName);
+        }
+        ArrayAdapter<String> adapter4 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, pageNames);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        pageSpinner.setAdapter(adapter4);
+
         // Set up onClickListener on "Submit" to bring user back to EditorActivity
-        buttonSetText = findViewById(R.id.buttonSetText);
-        buttonSetText.setOnClickListener(new View.OnClickListener() {
+        buttonSetTxt = findViewById(R.id.buttonSetText);
+        buttonSetTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onSubmit(view); // Create new textShape
-                System.out.println("game's current page is " + game.currentPage.getPageName());
-
-                Intent intent = new Intent(TextShapeActivity.this, EditorActivity.class);
-                startActivity(intent); // Direct user back to EditorActivity
+                onSubmit(view); // Create new rectShape
+                if(errorCheck()) {
+                    Intent intent = new Intent(TextShapeActivity.this, EditorActivity.class);
+                    startActivity(intent); // Direct user back to EditorActivity
+                }
             }
         });
 
         // Set up onClickListener on "Cancel" to bring user back to EditorActivity
-        buttonCancelText = findViewById(R.id.buttonCancelText);
-        buttonCancelText.setOnClickListener(new View.OnClickListener() {
+        buttonCancelTxt = findViewById(R.id.buttonCancelText);
+        buttonCancelTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                System.out.println("game's current page is " + game.currentPage.getPageName());
+
                 Intent intent = new Intent(TextShapeActivity.this, EditorActivity.class);
                 startActivity(intent);
+
             }
+
         });
     }
 
     public void onSubmit(View view) {
-        EditText inputTextShapeName = findViewById(R.id.inputTextShapeName);
-        EditText inputText = findViewById(R.id.inputText);
-        EditText inputFontSize = findViewById(R.id.inputFontSize);
-        Spinner inputTextPage = findViewById(R.id.inputTextPage); // TODO: Need to set up Spinner/ArrayAdapater to display list of pages
-        CheckBox checkboxTextIsHidden = findViewById(R.id.checkboxTextIsHidden);
-        CheckBox checkboxTextIsMovable = findViewById(R.id.checkboxTextIsMovable);
-        CheckBox checkboxTextIsInventory = findViewById(R.id.checkboxTextIsInventory);
-        EditText editTextXPosition = findViewById(R.id.editTextXPosition);
-        EditText editTextYPosition = findViewById(R.id.editTextYPosition);
-        EditText editTextWidth = findViewById(R.id.editTextWidth);
-        EditText editTextHeight = findViewById(R.id.editTextHeight);
-        //EditText shapeScript = findViewById(R.id.script);
+        EditText inputTxtShapeName = findViewById(R.id.inputTxtShapeName);
+        EditText addedText = findViewById(R.id.inputText);
+        EditText inputFontSz = findViewById(R.id.fontSize);
+        Spinner inputTxtPage = findViewById(R.id.inputTextPage);
+        CheckBox checkboxTxtIsHidden = findViewById(R.id.checkboxRectIsHidden);
+        CheckBox checkboxTxtIsMovable = findViewById(R.id.checkboxRectIsMovable);
+        CheckBox checkboxTxtIsInventory = findViewById(R.id.checkboxRectIsInventory);
+        EditText editTxtLeftPosition = findViewById(R.id.editRectLeftPosition);
+        EditText editTxtRightPosition = findViewById(R.id.editRectRightPosition);
+        EditText editTxtTopPosition = findViewById(R.id.editRectTopPosition);
+        EditText editTxtBottomPosition = findViewById(R.id.editRectBottomPosition);
 
-        float x = Float.parseFloat(String.valueOf(editTextXPosition.getText()));
-        float y = Float.parseFloat(String.valueOf(editTextYPosition.getText()));
-        // TODO: if the user wants the item to be in Inventory, then we should throw an error if the (x,y) is invalid (i.e. not within the Inventory box)
+        Spinner actionSpinner = findViewById(R.id.acSpin3);
+        Spinner eventSpinner = findViewById(R.id.evSpin3);
+        EditText scriptScript = findViewById(R.id.txtScript);
+        EditText scriptTarget = findViewById(R.id.txtTarget);
 
-        float width = Float.parseFloat(String.valueOf(editTextWidth.getText()));
-        float height = Float.parseFloat(String.valueOf(editTextHeight.getText()));
+        String script = "";
+        if(eventSpinner.getSelectedItemPosition()!= 0){
+            script = eventSpinner.getSelectedItem().toString();
 
-        TextShape newText = new TextShape(inputText.getText().toString(),
-                Integer.parseInt(inputFontSize.getText().toString()),
-                inputTextShapeName.getText().toString(),
-                checkboxTextIsHidden.isChecked(),
-                checkboxTextIsMovable.isChecked(),
-                checkboxTextIsInventory.isChecked(),
-                "shape script",
-                x, y, width, height);
-        //String destination = inputTextPage.getText().toString(); // TODO: Need to get Page instead of string
+            if(script.equals("on drop")){
+                script += " ";
+                script += scriptTarget.getText().toString();
+            }
+            script += " ";
+            script += actionSpinner.getSelectedItem().toString();
+            script += " ";
+            script += scriptScript.getText().toString();
+            script += ";";
 
-        if (checkboxTextIsInventory.isChecked()) {
-            game.addInventory(newText);
-        } else {
-            game.getCurrentPage().addShape(newText); //TODO: Need to pass in user's page selection
         }
-        System.out.println("Singleton after adding image " + game.getCurrentPage().getNumShapesOnPage());
+
+        String txt = addedText.getText().toString();
+        String fontSize = inputFontSz.getText().toString();
+
+
+        String xs1 = editTxtLeftPosition.getText().toString();
+        String xs2 = editTxtRightPosition.getText().toString();
+        String ys1 = editTxtTopPosition.getText().toString();
+        String ys2 = editTxtBottomPosition.getText().toString();
+        int x1 = Integer.valueOf(xs1);
+        int x2 = Integer.valueOf(xs2);
+        int y1 = Integer.valueOf(ys1);
+        int y2 = Integer.valueOf(ys2);
+
+        int width = x2-x1;
+        int height = y2-1;
+
+        TextShape newTxt = new TextShape(txt,
+                Integer.valueOf(fontSize),
+                inputTxtShapeName.getText().toString(),
+                checkboxTxtIsHidden.isChecked(),
+                checkboxTxtIsMovable.isChecked(),
+                checkboxTxtIsInventory.isChecked(),
+                script, (float)x1, (float)y1, (float)width, (float)height); // TODO: Replace later
+
+
+
+        //Page to add to
+        Page addTo = null;
+        String destination = inputTxtPage.getSelectedItem().toString();
+        //loop through the page list to find page with same name
+        for(Page p: game.getPageList()){
+            if (p.getPageName().equals(destination)){
+                addTo = p;
+            }
+        }
+
+        //If there is not page with this name
+        if(addTo.equals(null)){
+            System.out.println("Not a valid page.");
+            return;
+        }
+
+        if (checkboxTxtIsInventory.isChecked()) {
+            game.addInventory(newTxt);
+        } else {
+            addTo.addShape(newTxt); //TODO: Need to pass in user's page selection
+        }
+        System.out.println("Singleton after adding rect " + addTo.getNumShapesOnPage());
+    }
+
+    public boolean errorCheck(){
+        EditText inputTxtShapeName = findViewById(R.id.inputTxtShapeName);
+        EditText addedText = findViewById(R.id.inputText);
+        EditText inputFontSz = findViewById(R.id.fontSize);
+        Spinner inputTxtPage = findViewById(R.id.inputTextPage);
+
+        if(inputTxtShapeName.getText().toString().equals("")){
+            Toast.makeText(this, "Must Give Shape Name", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if(addedText.getText().toString().equals("")){
+            Toast.makeText(this, "Must Enter Text", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(inputFontSz.getText().toString().equals("")){
+            Toast.makeText(this, "Must Set Font Size", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(inputTxtPage.getSelectedItemPosition() == 0){
+            Toast.makeText(this, "Must Select Page", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+
+        return true;
     }
 }
